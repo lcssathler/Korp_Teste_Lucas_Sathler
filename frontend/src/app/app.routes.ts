0f4a32bfb27deee456bd07e5f8bf84f671d7696f
lcssathler <lcssathler@gmail.com>
2025-11-06 17:22:48 -0300

    creating modules and services

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  {
+    path: 'products',
+    loadComponent: () => import('./features/products/product-list/product-list.component').then(m => m.ProductListComponent)
+  },
+  {
+    path: 'products/new',
+    loadComponent: () => import('./features/products/product-form/product-form.component').then(m => m.ProductFormComponent)
+  },
+  {
+    path: 'invoices',
+    loadComponent: () => import('./features/invoices/invoice-list/invoice-list.component').then(m => m.InvoiceListComponent)
+  },
+  {
+    path: 'invoices/new',
+    loadComponent: () => import('./features/invoices/invoice-form/invoice-form.component').then(m => m.InvoiceFormComponent)
+  },
+  {
+    path: 'invoices/:id',
+    loadComponent: () => import('./features/invoices/invoice-detail/invoice-detail.component').then(m => m.InvoiceDetailComponent)
+  },
+  { path: '**', redirectTo: 'products' }
+];
---
+++ frontend/src/app/features/invoices/invoice-detail/invoice-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-detail',
+  imports: [],
+  templateUrl: './invoice-detail.component.html',
+  styleUrl: './invoice-detail.component.scss'
+})
+export class InvoiceDetailComponent {
+
+}

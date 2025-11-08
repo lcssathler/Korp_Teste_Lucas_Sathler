b143e7977e61356ed68f14f35220f72192d232de
lcssathler <lcssathler@gmail.com>
2025-11-08 11:37:07 -0300

    create product list component

--- frontend/src/app/features/product/product-list/product-list.component.ts
+++ frontend/src/app/features/product/product-list/product-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { Product } from '../../../models/product.model';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, MatButtonModule, MatTableModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  displayedColumns = ['code', 'description', 'balance'];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService
+      .getAll()
+      .subscribe((products) => (this.products = products));
+  }
 }

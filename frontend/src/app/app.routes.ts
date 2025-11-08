e6415d633792d0544eb77e061dff53a8c755a11e
lcssathler <lcssathler@gmail.com>
2025-11-08 11:08:08 -0300

    new structure: create components using standalone components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,26 +1,14 @@
 import { Routes } from '@angular/router';
 
+
 export const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   {
     path: 'products',
-    loadComponent: () => import('./features/products/product-list/product-list.component').then(m => m.ProductListComponent)
-  },
-  {
-    path: 'products/new',
-    loadComponent: () => import('./features/products/product-form/product-form.component').then(m => m.ProductFormComponent)
+    loadChildren: () => import('./features/product/product.routes').then(r => r.PRODUCT_ROUTES)
   },
   {
     path: 'invoices',
-    loadComponent: () => import('./features/invoices/invoice-list/invoice-list.component').then(m => m.InvoiceListComponent)
-  },
-  {
-    path: 'invoices/new',
-    loadComponent: () => import('./features/invoices/invoice-form/invoice-form.component').then(m => m.InvoiceFormComponent)
-  },
-  {
-    path: 'invoices/:id',
-    loadComponent: () => import('./features/invoices/invoice-detail/invoice-detail.component').then(m => m.InvoiceDetailComponent)
-  },
-  { path: '**', redirectTo: 'products' }
+    loadChildren: () => import('./features/invoice/invoice.routes').then(r => r.INVOICE_ROUTES)
+  }
 ];

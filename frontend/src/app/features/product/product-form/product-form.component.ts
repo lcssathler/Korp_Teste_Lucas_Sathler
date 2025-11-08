885872328b2b2744472bfb4d62e9d274dfb698df
lcssathler <lcssathler@gmail.com>
2025-11-08 13:44:14 -0300

    create product-form

--- frontend/src/app/features/product/product-form/product-form.component.ts
+++ frontend/src/app/features/product/product-form/product-form.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ProductService } from '../../../services/product.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-product-form',
-  imports: [],
-  templateUrl: './product-form.component.html',
-  styleUrl: './product-form.component.scss'
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],  templateUrl: './product-form.component.html',
+  styleUrl: './product-form.component.scss',
 })
-export class ProductFormComponent {
 
+export class ProductFormComponent {
+  fb: FormBuilder = new FormBuilder();
+
+  productForm = this.fb.group({
+    code: ['', Validators.required],
+    description: ['', Validators.required],
+    balance: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(
+    private productService: ProductService,
+    private router: Router
+  ) {}
+
+  onSubmit(): void {
+    if (this.productForm.valid) {
+      this.productService
+        .create(this.productForm.value as any)
+        .subscribe(() => this.router.navigate(['/products']));
+    }
+  }
 }
